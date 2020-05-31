efa09949359b1f79bf1b2f8a2df83b04578f6732
jennifer <jennifer249@163.com>
2020-06-01 00:09:41 +0800

    后端结构修改

--- node_web/index.js
+++ node_web/index.js
@@ -1,58 +1,116 @@
+// 连接 MySQL：先安装 npm i mysql -D
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '19961209',
+	database: 'blog'
+});
+
+connection.connect();
+
+//引入相关模块
 let http = require("http");
-
 let fs = require("fs");
-
 let url = require("url");
-
 let path = require("path");
+let qs = require("querystring");
 
 http.createServer((req, res) => {
 
-	let pathName = url.parse(req.url).pathname; //转换为url对象
-
-	//默认加载路径
-	if(pathName == '/') {
-		pathName = "/index.html";
+	switch (req.method) {
+		case 'POST':
+			postMehod(req, res);
+			break;
+		case 'GET':
+		default:
+			getMethod(req, res);
+			break;
 	}
 
-	//获取文件后缀名
-	let extName = path.extname(pathName);
-
-	// console.log("../fore_end" + pathName);  //例如../fore_end/css/style.css
-	fs.readFile("../fore_end" + pathName, (err, data) => {
-		console.log("../fore_end" + pathName);
-		if(err) { //出错则返回404页面
-			console.log("\n 404 Not Found!");
-			console.log("../fore_end" + pathName);
-			fs.readFile("../fore_end/404.html", (errorNotFound, dataNotFound) => {
-				if(errorNotFound) {
-					console.log(errorNotFound);
-				} 
-				else {
-					res.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
-
-					res.write(dataNotFound); //返回404页面
-
-					res.end();
-				}
-			})
-			return;
-		}
-		else {
-			// 获取对应后缀的文件类型
-			let ext = getExt(extName);
-
-			// 设置请求头
-			res.writeHead(200, {"Content-Type": ext + "; charset=utf-8"});
-
-			res.write(data); //返回请求文件
-			res.end();
-		}
-	})
 }).listen(8888);
 
 getExt = (extName) => {
 	let data = fs.readFileSync("./ext.json");
 	let ext = JSON.parse(data.toString()); //转换为js对象
 	return ext[extName];
+}
+
+function getMethod(req, res) {
+	console.log('\n[GET请求]');
+	let pathName = url.parse(req.url).pathname; //转换为url对象
+	let basePath = "../fore_end";
+	//默认加载路径
+	if (pathName == '/') {
+		pathName = "/index.html";
+	}
+
+	//获取文件后缀名
+	let extName = path.extname(pathName);
+
+	//例如../fore_end/css/style.css
+	fs.readFile(path.join(basePath, pathName), (err, data) => {
+		//出错则返回404页面
+		if (err) { 
+			console.log("404 Not Found! " + path.join(basePath,pathName));
+			fs.readFile(path.join(basePath,"404.html"), (errorNotFound, dataNotFound) => {
+				if (errorNotFound) {
+					console.log(errorNotFound);
+				} 
+				else {
+					res.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
+					res.write(dataNotFound); //返回404页面
+					res.end();
+				}
+			})
+			return;
+		}
+
+		// 获取对应后缀的文件类型
+		let ext = getExt(extName);
+		// 设置请求头
+		res.writeHead(200, {"Content-Type": ext + "; charset=utf-8"});
+		res.end(data);//返回请求文件
+	})
+}
+
+function postMethod(req, res) {
+	console.log('\n[POST请求]');
+	let pathName = req.url;
+	console.log("post请求的接口:");
+	console.log(pathName);
+
+	//数据接收中
+	req.on('data', function(chunk) {
+		tempResult += chunk;
+	});
+
+	//数据接收完成
+	req.on('end', function() {
+		//将post的请求体解析为查询字符串的值键对集合，最后转换为JSON字符串
+		var result = JSON.stringify(qs.parse(tempResult));
+		console.log("result数据:");
+		console.log(result);
+
+		if (pathName == '/login') {
+			console.log("\n[/login]");
+			result = JSON.parse(result); //转换为Javascript对象
+
+			let selectSql = "SELECT * FROM user_info where user_name = " + result.username + " and " + result.userpassword;
+			connection.query(selectSql, function(err, data) {
+				if (err) {
+					console.log(err);
+				}
+				else if (data == undefined || data.length == 0) {
+					res.end("用户名或密码错误！");
+					return;
+				}
+				else {  //登陆成功
+					res.write(JSON.stringify({
+						code: ''
+					}))
+				}
+			})
+		}
+	})
 }
